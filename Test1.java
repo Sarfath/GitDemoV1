df6d19d8c9f7d1b32fe4dfbe0107e8534e8adfa4
Lenovo <toarfath@gmail.com>
2023-07-25 15:50:41 +0530

    This is my first commit...

---
+++ Test1.java
@@ -0,0 +1,22 @@
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "Yes";
+
+		
+			if (hungry == "yes") {
+
+				System.out.println("Eat something ");
+
+			} 
+			
+			else {
+				System.out.println("Do work");
+
+			}
+		}
+
+
+	}
